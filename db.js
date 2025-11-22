376b08d63a1f86574daf0e7fe5ddb8c9fa94b8a5
Nitesh-Codez <kushwahnitesh388@gmail.com>
2025-11-22 09:02:42 +0530

    Fix db connection, use db.js in authController

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const mysql = require("mysql2");
 
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
+  port: process.env.DB_PORT,          // Port add karna sahi hai
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME

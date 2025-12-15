8b1ddaacc3c9b53ec319a30fc410005d43e817a3
Nitesh-Codez <kushwahnitesh388@gmail.com>
2025-12-15 22:19:19 +0530

    see

--- routes/attendanceRoutes.js
+++ routes/attendanceRoutes.js
@@ -6,33 +6,22 @@ const attendanceController = require("../controllers/attendanceController");
 
 // ------------------------------------
 // GET students list for a specific date (Admin)
-// ------------------------------------
 router.get("/list", attendanceController.getStudentsList);
 
 // ------------------------------------
 // POST mark or update attendance (Admin)
-// ------------------------------------
 router.post("/mark", attendanceController.markAttendance);
 
 // ------------------------------------
-// GET full attendance of a student (Student)
-// ------------------------------------
-router.get("/:id", attendanceController.getStudentAttendance);
-
-// ------------------------------------
 // GET today attendance percentage (Admin)
-// ------------------------------------
-router.get(
-  "/today-percent",
-  attendanceController.getTodayAttendancePercent
-);
+router.get("/today-percent", attendanceController.getTodayAttendancePercent);
 
 // ------------------------------------
 // ✅ GET attendance marks (MONTHLY)
+router.get("/attendance-marks", attendanceController.getAttendanceMarks);
+
 // ------------------------------------
-router.get(
-  "/attendance-marks",
-  attendanceController.getAttendanceMarks
-);
+// GET full attendance of a student (Student)
+router.get("/:id", attendanceController.getStudentAttendance);
 
 module.exports = router;

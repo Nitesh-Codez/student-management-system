91001db401df7b0d28453559932a9dfcbebb82be
Nitesh-Codez <kushwahnitesh388@gmail.com>
2026-01-30 12:38:59 +0530

    proper

--- middlewares/headAuth.js
+++ middlewares/headAuth.js
@@ -1,21 +1,22 @@
 const headAuth = (req, res, next) => {
   const secret = req.headers["x-head-secret"];
 
-  if (!secret) {
-    return res.status(401).json({
-      success: false,
-      message: "Secret key required"
-    });
-  }
+ if (!secret) {
+  return res.status(401).json({
+    success: false,
+    message: "Key missing. Ask permission from Admin."
+  });
+}
 
-  if (secret !== process.env.HEAD_SECRET_KEY) {
-    return res.status(403).json({
-      success: false,
-      message: "Only Head can edit marks"
-    });
-  }
+if (secret !== process.env.HEAD_SECRET_KEY) {
+  return res.status(403).json({
+    success: false,
+    message: "Invalid key. Ask permission from Admin."
+  });
+}
 
-  next();
+next();
+
 };
 
 module.exports = headAuth;

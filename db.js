2539ae8324610c937571458e3d29213ffa840313
Nitesh-Codez <kushwahnitesh388@gmail.com>
2026-02-04 09:47:48 +0530

    jk

--- db.js
+++ db.js
@@ -1,16 +1,18 @@
 const { Pool } = require("pg");
 
+// Pool config
 const db = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
-    rejectUnauthorized: false
-  }
+    rejectUnauthorized: false, // ✅ ye line must hai
+  },
 });
 
+// Test connection
 (async () => {
   try {
-    await db.query("SELECT NOW()");
-    console.log("✅ PostgreSQL Connected Successfully!");
+    const res = await db.query("SELECT NOW()");
+    console.log("✅ PostgreSQL Connected Successfully at:", res.rows[0].now);
   } catch (error) {
     console.error("❌ DB Connection Error FULL:", error);
   }

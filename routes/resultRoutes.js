2cef5c188999bf1dce37914dce8e43d7fef0f783
Nitesh-Codez <kushwahnitesh388@gmail.com>
2026-02-22 13:08:41 +0530

    ko

--- routes/resultRoutes.js
+++ routes/resultRoutes.js
@@ -2,14 +2,15 @@ const express = require("express");
 const router = express.Router();
 const quizController = require("../controllers/quizController");
 
-// Admin Routes
+// ================== Admin Routes ==================
 router.post("/create", quizController.createQuiz);
 router.get("/admin/results/:class_name", quizController.getAdminResults);
 
-// Student Routes
+// ================= Student Routes =================
+// Note: "/status" should come before "/:id" to avoid route conflict
 router.get("/class/:class_name", quizController.getQuizByClass);
-router.get("/:id", quizController.getSingleQuiz);
 router.get("/status/:quizId/:studentId", quizController.checkAttemptStatus);
+router.get("/:id", quizController.getSingleQuiz);
 router.post("/submit", quizController.submitQuiz);
 
 module.exports = router;

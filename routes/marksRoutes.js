3388b61232c99c2d74a71d44d53fcc0230dbdd44
Nitesh-Codez <kushwahnitesh388@gmail.com>
2026-08-09 13:13:14 +0530

    p

--- routes/marksRoutes.js
+++ routes/marksRoutes.js
@@ -1,44 +1,39 @@
 const express = require("express");
 const router = express.Router();
+//Middlewares
 const headAuth = require("../middlewares/headAuth");
+const authMiddleware = require("../middlewares/authMiddleware");
+const adminMiddleware = require("../middlewares/adminMiddleware");
 
-
+//Controllers
 const marksController = require("../controllers/marksController");
 
-
 // ================= GET CLASSES =================
-router.get("/classes", marksController.getClasses);
-
+router.get("/classes",authMiddleware, marksController.getClasses);
 
 // ================= GET STUDENTS BY CLASS =================
-router.get("/students/:className", marksController.getStudentsByClass);
-
+router.get("/students/:className",authMiddleware, marksController.getStudentsByClass);
 
 // ================= ADD MARKS =================
-router.post("/add", marksController.addMarks);
-
+router.post("/add",authMiddleware,adminMiddleware, marksController.addMarks);
 
 // ================= CHECK MARKS (STUDENT) =================
-router.post("/check", marksController.checkMarks);
-
+router.post("/check",authMiddleware, marksController.checkMarks);
 
 // ================= DELETE MARKS (ADMIN) =================
-router.delete("/admin/marks/:id", marksController.deleteMarks);
-
-
+router.delete("/admin/marks/:id",authMiddleware,adminMiddleware, marksController.deleteMarks);
 
 // ================= UPDATE MARKS (ADMIN) =================
 router.put(
  "/admin/marks/:id",
+ authMiddleware,
+ adminMiddleware,
 headAuth, // 🔐 protection
 marksController.updateMarks
 );
 
-
 // ================= GET ALL MARKS (ADMIN) =================
-router.get("/admin/marks", marksController.getAllMarks);
-
-
+router.get("/admin/marks",authMiddleware,adminMiddleware, marksController.getAllMarks);
 
 
 module.exports = router;

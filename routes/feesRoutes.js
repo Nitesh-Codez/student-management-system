e4393c536a36fb122e55f64c957fc6993068b3fe
Nitesh-Codez <kushwahnitesh388@gmail.com>
2026-09-13 16:35:53 +0530

    fkfk

--- routes/feesRoutes.js
+++ routes/feesRoutes.js
@@ -9,23 +9,25 @@ const {
   updateFee,
   deleteFee,
 } = require("../controllers/feesController");
+const authMiddleware = require("../middlewares/authMiddleware");
+const adminMiddleware = require("../middlewares/adminMiddleware");
 
 //=======================================
 // ADMIN SIDE 
 
 // GET all fees
-router.get("/all-fee", getAllFees);
+router.get("/all-fee",authMiddleware,adminMiddleware, getAllFees);
 
 // GET single fee
-router.get("/:id", getFeeById);
+router.get("/:id",authMiddleware,getFeeById);
 
 // ADD fee
-router.post("/add", addFee);
+router.post("/add",authMiddleware,adminMiddleware, addFee);
 
 // UPDATE fee
-router.put("/update/:id", updateFee);
+router.put("/update/:id",authMiddleware,adminMiddleware, updateFee);
 
 // DELETE fee
-router.delete("/delete/:id", deleteFee);
+router.delete("/delete/:id",authMiddleware,adminMiddleware, deleteFee);
 
 module.exports = router;
